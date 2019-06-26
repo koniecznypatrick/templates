23d0dd16d5657a4143cd7350eac6dd8684094f9d
patrick Konieczny <pkonieczny@apside-groupe.com>
2019-06-26 16:41:35 +0200

    first commit

---
+++ src/com/apside/template/ws/soap/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.apside.template.ws.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://soap.ws.template.apside.com/", portName = "HelloWorldPort", serviceName = "HelloWorldService")
+public class HelloWorld {
+
+	public String sayHello(String name) {
+		return "Hello "+name;
+	}
+	
+	public String ping() {
+		return "pong";
+	}
+}
