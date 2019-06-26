6f58932f494410c9be6757c09159a2e0df3013f2
patrick Konieczny <pkonieczny@apside-groupe.com>
2019-06-26 17:09:44 +0200

    fix : ajout des annotation webmethod

--- src/com/apside/template/ws/soap/HelloWorld.java
+++ src/com/apside/template/ws/soap/HelloWorld.java
@@ -6,10 +6,12 @@ import javax.jws.WebService;
 @WebService(targetNamespace = "http://soap.ws.template.apside.com/", portName = "HelloWorldPort", serviceName = "HelloWorldService")
 public class HelloWorld {
 
+	@WebMethod
 	public String sayHello(String name) {
 		return "Hello "+name;
 	}
 	
+	@WebMethod
 	public String ping() {
 		return "pong";
 	}
